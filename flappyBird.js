a9b11b197765b7d1fc81d232eadb8a9116bff065
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-06-24 23:25:32 +0100

    Loaded images on canvas

--- flappyBird.js
+++ flappyBird.js
@@ -2,10 +2,35 @@ let cvs = document.getElementById("canvas"); //select the canvas
 let ctx = cvs.getContext("2d");
 
 
-//Load canvas
+//Load images
+let bird = new Image();
 let bg = new Image();
-bg.src = "images/background.png";
+let fg = new Image();
+let pipeNorth = new Image();
+let pipeSouth =new Image();
 
-//Draw canvas
-ctx.drawImage(bg, 0, 0)
+bird.src = "images/bird.png";
+bg.src = 'images/background.png';
+fg.src = "images/fallground.png";
+pipeNorth.src = "images/pipeNorth.png";
+pipeSouth.src = "images/pipeSouth.png";
 
+const gap = 85; //space between north and south pipes
+let pSouthHeight = pipeNorth.height+gap;
+
+let bX = 10; //x position of bird
+let bY = 150; //y position of bird
+
+//Draw Images
+function draw(){
+    ctx.drawImage(bg,0,0); //draws the background
+    
+    ctx.drawImage(pipeNorth, 100, 0); //draws the north pipe
+    ctx.drawImage(pipeSouth, 100, pSouthHeight); //draws the south pipe
+
+    ctx.drawImage(fg, 0, cvs.height - fg.height); //draws the fallground
+
+    ctx.drawImage(bird, bX, bY); //draws the bird
+}
+
+draw(); //calls the draw function
