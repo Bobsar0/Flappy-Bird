3312322b103753d46d360571e284cb8109610ad4
Steve Onyeneke <s.c.onyeneke.09@aberdeen.ac.uk>
2018-06-24 23:57:21 +0100

    Pipes moving to the left

--- flappyBird.js
+++ flappyBird.js
@@ -21,20 +21,42 @@ let pSouthHeight = pipeNorth.height+gap;
 let bX = 10; //x position of bird
 let bY = 150; //y position of bird
 
-let gravity = 1;
+let gravity = 1.5;
 
 //When any keyboard key is pressed down, bird goes up
 document.addEventListener("keydown", moveUp);
 function moveUp(){
-    bY -=20;
+    bY -=25;
+}
+
+//Declare pipe coordinates
+let pipe = [];
+
+pipe[0] = {
+    x : cvs.width,
+    y : 0
 }
 
 //Draw Images
 function draw(){
     ctx.drawImage(bg,0,0); //draws the background
-    
-    ctx.drawImage(pipeNorth, 100, 0); //draws the north pipe
-    ctx.drawImage(pipeSouth, 100, pSouthHeight); //draws the south pipe
+
+   //Move pipes to the left and draw new pipes
+    for (let i = 0; i < pipe.length; i++){
+        ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y); //draws the north pipe
+        ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y+pSouthHeight); //draws the south pipe
+
+        pipe[i].x--; //move pipes to the left
+
+        //To draw new pipes
+        if (pipe[i].x === 125){
+            pipe.push({ //push new pipe object to the pipe array
+                x : cvs.width,
+                y : Math.floor(Math.random()*pipeNorth.height) - pipeNorth.height //random position
+            })
+        }
+    }    
+ 
 
     ctx.drawImage(fg, 0, cvs.height - fg.height); //draws the fallground
 
